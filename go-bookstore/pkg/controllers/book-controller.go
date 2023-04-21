22c50c90f19337553bbd2ebdf92ce476ff54bbc7
Norbert Gruszka <ngru95@outlook.com>
2023-04-21 16:19:30 +0200

    finish DB app

--- go-bookstore/pkg/controllers/book-controller.go
+++ go-bookstore/pkg/controllers/book-controller.go
@@ -11,3 +11,63 @@ import (
 )
 
 var NewBook models.Book
+
+func GetBooks(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	if id := chi.URLParam(r, "bookId"); id != "" {
+		ID, err := strconv.ParseInt(id, 0, 0)
+		if err != nil {
+			fmt.Println("error while parsing")
+		}
+		bookDetails, _ := models.GetBookById(ID)
+		res, _ := json.Marshal(bookDetails)
+		w.Header().Set("Content-Type", "pkglication/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(res)
+	}
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeletBook(w http.ResponseWriter, r *http.Request) {
+	if id := chi.URLParam(r, "bookId"); id != "" {
+		ID, err := strconv.ParseInt(id, 0, 0)
+		if err != nil {
+			fmt.Println("error while parsing")
+		}
+		book := models.DeleteBook(ID)
+		res, _ := json.Marshal(book)
+		w.Header().Set("Content-Type", "pkglication/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(res)
+	}
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	updateBook := &models.Book{}
+	utils.ParseBody(r, updateBook)
+
+	if id := chi.URLParam(r, "bookId"); id != "" {
+		ID, err := strconv.ParseInt(id, 0, 0)
+		if err != nil {
+			fmt.Println("error while parsing")
+		}
+
+		bookDetails, db := models.GetBookById(ID)
+	}
+}
